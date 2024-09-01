08da456028183dc59ce2e6dc161f5c50e81e92c2
Lado Asambadze <lado.asambadze1@gmail.com>
2024-09-01 11:09:24 +0400

    add domain

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -4,5 +4,5 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
-  url: env("PUBLIC_URL", "https://your-production-domain.com"),
+  url: env("PUBLIC_URL", "http://207.154.214.237:1337"),
 });
