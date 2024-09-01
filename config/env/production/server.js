85dae103e7d7d322126c8f532b34c6f53eeb0ae4
Lado Asambadze <lado.asambadze1@gmail.com>
2024-09-01 11:32:35 +0400

    app keys

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,7 +2,11 @@ module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: [
+      "your_very_secret_key_1",
+      "another_very_secret_key_2",
+      "and_a_third_very_secret_key_3",
+    ],
   },
   url: env("PUBLIC_URL", "http://207.154.214.237:1337"),
 });
--- config/server.js
+++ config/server.js
@@ -1,13 +1,14 @@
-module.exports = ({ env }) => {
-  console.log(env.array("APP_KEYS")); // Add this line to debug
-  return {
-    host: env("HOST", "0.0.0.0"),
-    port: env.int("PORT", 1337),
-    app: {
-      keys: env.array("APP_KEYS"),
-    },
-    webhooks: {
-      populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-    },
-  };
-};
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: [
+      "your_very_secret_key_1",
+      "another_very_secret_key_2",
+      "and_a_third_very_secret_key_3",
+    ],
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+});
