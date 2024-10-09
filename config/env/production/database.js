771759b8bf5825ea2b921a7200953771d927600d
Lado Asambadze <lado.asambadze1@gmail.com>
2024-10-09 20:01:18 +0400

    update database

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "my_database"),
-      user: env("DATABASE_USERNAME", "lado"),
-      password: env("DATABASE_PASSWORD", "Key12345!"),
+      database: env("DATABASE_NAME", "postgres"),
+      user: env("DATABASE_USERNAME", "app"),
+      password: env("DATABASE_PASSWORD", "kgAZkCW1Z5043gQAbiVu"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
